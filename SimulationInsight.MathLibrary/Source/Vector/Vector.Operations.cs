f90eef2624b11a20ca08db8c50eabec0fef4bcea
Simulation Insight <simulationinsight@outlook.com>
2024-11-06 18:25:06 +0000

    #1: Added initial project and code.

---
+++ SimulationInsight.MathLibrary/Source/Vector/Vector.Operations.cs
@@ -0,0 +1,108 @@
+﻿namespace SimulationInsight.MathLibrary;
+
+public partial class Vector
+{
+    public double GetCircularValue(int index)
+    {
+        var i = index % Data.Length;
+
+        var x = Data[i];
+
+        return x;
+    }
+
+    public static Vector LinearlySpacedVector(double start, double end, double step)
+    {
+        var span = end - start;
+
+        int numberOfSamples = (int)(span / step);
+
+        var result = new Vector(numberOfSamples);
+
+        for (int i = 0; i < numberOfSamples; i++)
+        {
+            result[i] = start + step * i;
+        }
+
+        return result;
+    }
+
+    public static Vector Cos(Vector x)
+    {
+        var data = new double[x.NumberOfElements];
+
+        for (int i = 0; i < x.NumberOfElements; i++)
+        {
+            data[i] = Math.Cos(x.Data[i]);
+        }
+
+        var result = new Vector(data);
+
+        return result;
+    }
+
+    public static Vector Sin(Vector x)
+    {
+        var data = new double[x.NumberOfElements];
+
+        for (int i = 0; i < x.NumberOfElements; i++)
+        {
+            data[i] = Math.Sin(x.Data[i]);
+        }
+
+        var result = new Vector(data);
+
+        return result;
+    }
+
+    public bool IsMonotonicallyIncreasing()
+    {
+        if (NumberOfElements < 2)
+        {
+            return true;
+        }
+
+        var delta = 0.0;
+
+        for (int i = 0; i < NumberOfElements - 1; i++)
+        {
+            delta = Data[i + 1] - Data[i];
+
+            if (delta < 0.0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsMonotonicallyDecreasing()
+    {
+        if (NumberOfElements < 2)
+        {
+            return true;
+        }
+
+        var delta = 0.0;
+
+        for (int i = 0; i < NumberOfElements - 2; i++)
+        {
+            delta = Data[i + 1] - Data[i];
+
+            if (delta > 0.0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsMonotonic()
+    {
+        var isMonotonic = IsMonotonicallyIncreasing() || IsMonotonicallyDecreasing();
+
+        return isMonotonic;
+    }
+}
